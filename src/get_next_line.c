49c93e4a69beb7ef3145a6874798b73f2b8a190a
Jairo Caetano <42660842+jjajairo@users.noreply.github.com>
2021-11-23 16:01:23 -0300

    Add libft on gnl

--- src/get_next_line.c
+++ src/get_next_line.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include "libft.h"
 
 static char	*ft_read_fd(int fd, char *stack_backup, char *heap_buffer);
 static char	*ft_format_line(char *line);
